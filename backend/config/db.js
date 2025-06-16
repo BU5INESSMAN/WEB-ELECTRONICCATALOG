66e64f9244ad71fbd9bd1ec0818a0e9a1725e7c3
Ряховский Михаил <sunflowerlmao@vk.com>
2025-06-16 11:34:50 +0700

    0_0

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,13 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Для бесплатных баз, таких как Render PostgreSQL
+    rejectUnauthorized: false // Разрешить небезопасное SSL-соединение для бесплатных баз
   }
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
