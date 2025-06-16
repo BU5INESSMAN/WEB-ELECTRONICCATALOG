6aaad17cfdb9262833a6907ca534cad22d5565a7
Ряховский Михаил <sunflowerlmao@vk.com>
2025-06-16 12:21:47 +0700

    Updated server.js to use auth and products routes

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 
 // Служба статических файлов фронтенда
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Обработка всех остальных маршрутов через React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Ваши API-роуты
-app.use('/api', require('./routes'));
+// Подключение маршрутов
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/products', require('./routes/products'));
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
