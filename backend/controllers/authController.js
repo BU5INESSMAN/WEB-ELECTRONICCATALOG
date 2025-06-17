ea8dfaa43a6f58ebbe094eeeb2456c7e7c61a041
Ряховский Михаил <sunflowerlmao@vk.com>
2025-06-17 16:00:29 +0700

    Rebuilt frontend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,20 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
+const register = async (req, res) => {
+  const { email, password, role } = req.body;
+  try {
+    const existingUser = await User.findByEmail(email);
+    if (existingUser) return res.status(400).json({ message: 'Пользователь уже существует' });
+    const password_hash = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, password_hash, role });
+    res.status(201).json(user);
+  } catch (error) {
+    console.error('Ошибка регистрации:', error);
+    res.status(500).json({ message: 'Ошибка сервера' });
+  }
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -15,4 +29,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login };
+module.exports = { register, login };
